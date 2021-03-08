4a422e42e9cc70600485c2b311c5f9fc653d02a6
Amy Haley <amy.haley@skybettingandgaming.com>
2021-03-08 17:42:18 +0000

    [table] table made and css added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tabs from "./components/Tabs";
 import "./App.css";
+import "./Table.css";
 
 class App extends Component {
     constructor() {
@@ -45,12 +46,71 @@ class App extends Component {
 
                 <p>Accessibility solutions showcased were selected using a questionnaire all Gaming Admin users were invited to complete.</p>
                 <p>The table below shows the results from the questionnaire.</p>
-                <p>Highlighted rows show that this solution is implemeted within this prototype.</p>
+                <p>Highlighted rows show that this solution is implemented within this prototype.</p>
+                <table>
+                    <tr>
+                        <th>Accessibility feature</th>
+                        <th>Range of ratings</th>
+                        <th>Average rating</th>
+                        <th>Priority</th>
+                    </tr>
+                    <tr>
+                        <td>Video captions</td>
+                        <td>1-10</td>
+                        <td>4.08</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>Video script</td>
+                        <td>1-7</td>
+                        <td>4.31</td>
+                        <td>8</td>
+                    </tr>
+                    <tr>
+                        <td>Screen magnification</td>
+                        <td>1-10</td>
+                        <td>6.08</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>Screen reader</td>
+                        <td>3-10</td>
+                        <td>6.08</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>High contrasting Colour Toggle</td>
+                        <td>4-10</td>
+                        <td>7.46</td>
+                        <td>2</td>
+                    </tr>
+                    <tr>
+                        <td>Ability to navigate with a keyboard</td>
+                        <td>5-10</td>
+                        <td>8.62</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>Read only summary page</td>
+                        <td>1-10</td>
+                        <td>6.31</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>Digital voice recorder</td>
+                        <td>1-10</td>
+                        <td>5.46</td>
+                        <td>6</td>
+                    </tr>
+                    </table>
        </div> 
-       <div label="View a list"> 
+       <div label="List"> 
          Add a  <em>list</em>! 
+         <h1>Accessibility Reading</h1>
+            <p>Key websites to aid in accessibility</p>
+                        
        </div> 
-       <div label="View assets"> 
+       <div label="Media"> 
          Add <em>assets</em>! 
        </div> 
      </Tabs> 
@@ -61,3 +121,5 @@ class App extends Component {
 }
 
 export default App;
+
+//tab help from https://www.digitalocean.com/community/tutorials/react-tabs-component
