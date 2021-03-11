c5bc89880918e53ab06b233699007e8954cc1203
Amy Haley <amy.haley@skybettingandgaming.com>
2021-03-11 14:53:09 +0000

    refactored folders

---
+++ src/components/tab/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tab = ({ children, index, isSelected }) => (
+    <div id={`tabpanel_${index}`}
+        name={`tabpanel_${index}`}
+        role="tabpanel"
+        aria-labelledby={`tab_${index}`}
+        aria-hidden={!isSelected()}
+    >{children}</div>
+)
+
+export default Tab
