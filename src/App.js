d5897107869d68b52f667be53176bbfbdb66a7f9
Amy Haley <amy.haley@skybettingandgaming.com>
2021-03-12 15:41:55 +0000

    image added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import Table from "./components/table"
 import List from "./components/lists"
+
+import { AccessibilityText } from "./components/text/index.js"
 import ContentWithColourPicker from "./components/Content/index.js"
-
-import Tabs from "./components/tabs"
 import Tab from "./components/tab"
-
+import Tabs from "./components/tabs"
+import W3Clogo from "./components/images/index.js"
 import YoutubeEmbed from "./components/youtube";
-
 
 import "../src/styles/App.css";
 import "../src/styles/Table.css";
 
-// const tableWithColourPicker = WithColourPicker(Table)
-
 const App = () => (
-    <Tabs>
-      <Tab title="Project">
-        <div></div>
-        < ContentWithColourPicker />
-      </Tab>
-      {/* Second tab */}
-      <Tab title="Reading">
-        <h2>Accessibility...who? what? why?</h2>
-        <h3>What is meant by accessibility?</h3>
-        <p>Description here....</p>
-        <List />
-      </Tab>
-      {/* Final tab */}
-      <Tab title="Standards">
-        <h2>Introduction to Web Accessibility and W3C Standards</h2>
-        <div className="App">
-      <p>Add video summary</p>
-      <YoutubeEmbed embedId="20SHvU2PKsM" />
-    </div>
-      </Tab>
-    </Tabs>
-  )
+  <Tabs>
+    <Tab title="Project">
+      <div></div>
+      < ContentWithColourPicker />
+    </Tab>
+    {/* Second tab */}
+    <Tab title="Reading">
+      < AccessibilityText />
+      <List />
+    </Tab>
+    {/* Final tab */}
+    <Tab title="Standards">
+      <h2>Introduction to Web Accessibility and W3C Standards</h2>
+      <W3Clogo />
+      <p>Add image summary</p>
+      <div className="App">
+        <YoutubeEmbed embedId="20SHvU2PKsM" />
+        <p>Add video summary</p>
+      </div>
+    </Tab>
+  </Tabs>
+)
 
-  export default App
-  
+export default App
+
