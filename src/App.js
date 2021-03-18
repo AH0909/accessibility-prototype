f026ecefa7c3c9fb45fd4a9f187fc8010716feae
Amy Haley <amy.haley@skybettingandgaming.com>
2021-03-18 23:35:31 +0000

    dark mode for the whole app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from "react";
 import List from "./components/lists"
+
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/darkMode/globalStyles.js";
+import { lightTheme, darkTheme } from "./components/darkMode//themes.js";
 
 import { AccessibilityText } from "./components/text/index.js"
 import ContentWithColourPicker from "./components/Content/index.js"
@@ -16,8 +21,44 @@ import "../src/styles/table.css";
 import '../src/styles/video.css';
 import "../src/styles/toggle.css";
 
-const App = () => (
-  <Tabs>
+// const App = () => (
+//   <Tabs>
+//     <Tab title="Project">
+//       < ContentWithColourPicker />
+//     </Tab>
+//     {/* Second tab */}
+//     <Tab title="Reading">
+//     <Toggle />
+//       < AccessibilityText />
+//       <List />
+//     </Tab>
+//     {/* Final tab */}
+//     <Tab title="Standards">
+//       <h2>Introduction to Web Accessibility and W3C Standards</h2>
+//       <W3Clogo />
+//       < VideoText />
+//       <Contact />
+//       <YoutubeEmbed aria-label="video" embedId="20SHvU2PKsM" />
+//       <p>Introduction to Web Accessibility and W3C Standards.</p>
+//     </Tab>
+//   </Tabs>
+// )
+
+// export default App
+
+const App = () => {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+}
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles/>
+        <div className="App">
+          <button onClick={themeToggler}>Switch Theme</button>
+          <Tabs>
     <Tab title="Project">
       < ContentWithColourPicker />
     </Tab>
@@ -34,10 +75,14 @@ const App = () => (
       < VideoText />
       <Contact />
       <YoutubeEmbed aria-label="video" embedId="20SHvU2PKsM" />
-      <p>Introduction to Web Accessibility and W3C Standards.</p>
+      <p> A Youtube Video to show you an Introduction to Web Accessibility and W3C Standards.</p>
     </Tab>
   </Tabs>
-)
+        </div>
+      </>
+    </ThemeProvider>
+    
+  );
+};
 
-export default App
-
+export default App;
--- src/components/lists/index.js
+++ src/components/lists/index.js
@@ -8,7 +8,7 @@ class List extends Component {
                 From my research I found the following pages useful.
                 <dl>
                     <dt><a href="https://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG) Overview</a></dt>
-                    <dd> This site provides you with the World Wide Web Consortium's guidleines on web accesibility; named WCAG (Web Content Accessibility Guidelines) </dd>
+                    <dd> This site provides you with the World Wide Web Consortium's guidelines on web accesibility; named WCAG (Web Content Accessibility Guidelines) </dd>
                     <br />
                     <dt><a href="https://www.a11yproject.com/">a11y project</a></dt>
                     <dd> The A11Y Project is a community-driven effort to make digital accessibility easier. This site provides you with open source code and information on making your applications accessible.</dd>
