2fb8633701a73e48eb41aa7008c31b871afee66d
Amy Haley <amy.haley@skybettingandgaming.com>
2021-03-18 15:36:28 +0000

    styling and table name

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContentWithColourPicker from "./components/Content/index.js"
 import Tab from "./components/tab"
 import Tabs from "./components/tabs"
 import W3Clogo from "./components/images/index.js"
+import { VideoText } from "./components/text"
 import YoutubeEmbed from "./components/youtube";
 
 import "../src/styles/App.css";
@@ -26,13 +27,8 @@ const App = () => (
     <Tab title="Standards">
       <h2>Introduction to Web Accessibility and W3C Standards</h2>
       <W3Clogo />
-      <p>W3C, known as the World Wide Web Consortium, is an international standards organisation for the Web.<br/>
-        W3C have prodcuced a set of standards for accessibility named Web Content Accessibility Guidelines (WCAG).<br/>
-        The most recent set of guidelines are WCAG 2.1 published in June 2018.   </p>
-        <p>As part of my project I have looked closely at navigation and colour.<br/>
-        For navigation, "Success Criterion 3.2.3 Consistent Navigation" is relevant. <br/>
-        For colour, "Understanding Success Criterion 1.4.3: Contrast (Minimum)" is relevant.</p>
-        <YoutubeEmbed embedId="20SHvU2PKsM" />
+      < VideoText />
+        <YoutubeEmbed aria-label= "video" embedId="20SHvU2PKsM" />
         <p>Introduction to Web Accessibility and W3C Standards.</p>
     </Tab>
   </Tabs>
