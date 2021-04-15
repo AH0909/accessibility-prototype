c880a003e3f7d0c3baf12c78865e47d9080fed5b
Amy Haley <amy.haley@skybettingandgaming.com>
2021-04-15 14:06:38 +0100

    unit tests

--- src/components/darkMode/index.test.js
+++ src/components/darkMode/index.test.js
@@ -7,13 +7,33 @@ it('renders the toggle', () => {
   enzyme.configure({ adapter: new Adapter() })
   const toggle = enzyme.shallow(<Toggle/>)
   expect(toggle.exists(".toggleButton")).toBe(true)
-  expect(toggle.exists(".sun")).toBe(true)
-  expect(toggle.exists(".moon")).toBe(false)
 });
 
-it('matches snapshot', () => {
+it('renders the moon', () => {
+  enzyme.configure({ adapter: new Adapter() })
+  const toggle = enzyme.mount(<Toggle theme={"light"}/>)
+  expect(toggle.exists(".moon")).toBe(true)
+});
+
+it('renders the sun', () => {
+  enzyme.configure({ adapter: new Adapter() })
+  const toggle = enzyme.mount(<Toggle theme={"dark"}/>)
+  expect(toggle.exists(".sun")).toBe(true)
+});
+
+it('the toggle can be clicked', () => {
+  const clickToggle=jest.fn()
+  enzyme.configure({ adapter: new Adapter() })
+  const toggle = enzyme.mount(<Toggle theme={"light"} toggleTheme={clickToggle}/>)
+  expect(toggle.exists(".toggleButton")).toBe(true)
+  expect(toggle.exists(".moon")).toBe(true)
+  toggle.find("button.toggleButton").simulate('click')
+  expect(clickToggle.mock.calls.length).toBe(1)
+});
+
+it('matches the snapshot', () => {
   const wrapper = enzyme.mount(
-    <Toggle/>,
+    <Toggle/>
   );
   expect(wrapper).toMatchSnapshot();
 });
